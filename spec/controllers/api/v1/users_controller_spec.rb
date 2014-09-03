06043982ee4392e253c4b11e47ddf23db7ce45af
Cuong Lam <thanhcuong1990@gmail.com>
2014-09-03 23:05:01 +0700

    Adds the user create endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -6,14 +6,51 @@ RSpec.describe Api::V1::UsersController, :type => :controller do
   describe "GET #show" do
     before(:each) do
       @user = FactoryGirl.create :user
-      get :show, id: @user.id, format: :json
+      get :show, id: @user.id
     end
 
     it "returns the information about a reporter on a hash" do
-      user_response = JSON.parse(response.body, symblize_names: true)
-      expect(user_presponse[:email].to eql @user.email)
+      user_response = json_response
+      expect(user_response[:email]).to eql @user.email
     end
 
     it { should respond_with 200 }
   end
+
+  describe "POST #create" do
+
+    context "when is successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }
+      end
+
+      it "renders the json representation for the user record just created" do
+        user_response = json_response
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        @invalid_user_attributes = { password: "12345678", password_confirmation: "12345678" } #notice I'm not including the email
+        post :create, { user: @invalid_user_attributes }
+      end
+
+      it "renders an errors json" do
+        user_response = json_response
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on whye the user could not be created" do
+        user_response = json_response
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
 end
