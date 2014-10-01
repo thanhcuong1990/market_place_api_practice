ba7931a0a65753d1f0cbdda78583912bf472ceae
Cuong Lam <thanhcuong1990@gmail.com>
2014-10-01 23:02:47 +0700

    Update db file

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
 
+  def index
+    respond_with User.all
+  end
+
   def show
     respond_with User.find_by_id(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  mount SabisuRails::Engine => "/sabisu_rails"
   devise_for :users
   # Api definition
   namespace :api, defaults: {format: 'json'}, constraints: { subdomain: 'api'}, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-      resources :users, :only => [:show, :create, :update, :destroy]
+      resources :users, :only => [:show, :create, :update, :destroy, :index]
     end
   end
 end
